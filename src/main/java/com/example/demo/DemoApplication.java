4009f7faf83278c67bb17576f52aa78a1d7a1866
zhuyu_zhizao <406663924@qq.com>
2022-10-28 11:33:28 +0800

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author: zy
+ * @date:  2022/10/28 11:32 AM
+ *
+ * @description: demo club
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
